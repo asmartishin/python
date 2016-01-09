379fff1ac87b051c8818bede1c6099a0ccbf679b
Alexey Martishin <alex.martishin@yandex.ru>
2016-01-10 00:41:43 +0300

    Update _call_by_object.py

--- _call_by_object.py
+++ _call_by_object.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 # In python, when you assign a value, you are assigning a name to an object. Objects can be mutable and immutable
 
-def foo(bar):
+def foo1(bar):
     bar.append(42)
     print(bar)
 
-a = []
-foo(a)
-print(a)
-
-def foo(bar):
+def foo2(bar):
     bar = 'new'
     print(bar)
 
-a = 'old'
-foo(a)
+a = []
+foo1(a)
 print(a)
 
+b = 'old'
+foo2(b)
+print(b)
+
+
 # another example. we are appending to the same object 
+
 x = [[]]*3
 x[0].append('a')
 x[1].append('b')
 x[2].append('c')
 x[0]=['d']
-
 print x
+
+#out: 
+#[['d'], ['a', 'b', 'c'], ['a', 'b', 'c']]
