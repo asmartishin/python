0c6dd95443bb83c845d219e044658ba24b58519d
Alexey Martishin <alex.martishin@yandex.ru>
2016-01-10 00:28:22 +0300

    Update _args_kwargs.py

--- _args_kwargs.py
+++ _args_kwargs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #!/usr/bin/env python3
 
 #*args allows you pass an arbitrary number of arguments to function (unknown number)
@@ -5,37 +8,23 @@
 
 import inspect
 
-def test_var_args(farg, *args):
+def args_pass(*args):
     print(inspect.stack()[0][3], locals())
-    print("formal arg:", farg)
-    for arg in args:
-        print("another arg:", arg)
 
-def test_var_kwargs(farg, **kwargs):
+def kwargs_pass(**kwargs):
     print(inspect.stack()[0][3], locals())
-    print("formal arg: ", farg)
-    for key in kwargs:
-        print("another keyword arg: %s: %s" % (key, kwargs[key]))
 
-def var_list_call(arg1, arg2, arg3):
+def args_kwargs_pass(fargs, *args, **kwargs):
     print(inspect.stack()[0][3], locals())
-    print("arg1:", arg1)
-    print("arg2:", arg2)
-    print("arg3:", arg3)
 
-def var_dic_call(arg1, arg2, arg3):
-    print(inspect.stack()[0][3], locals())
-    print("arg1:", arg1)
-    print("arg2:", arg2)
-    print("arg3:", arg3)
+list_test = (1, 'two', 3) #it can be an array
+dict_test = {'arg1': 1, 'arg2': 2, 'arg3': 'three'}
 
-var_dic = {"arg3": 3, "arg2": "two"}
-var_list = ("two", 3)
+args_pass(*list_test)
+kwargs_pass(**dict_test)
+args_kwargs_pass(1, *list_test, **dict_test)
 
-var_list_call(1, *var_list)
-var_list_call(*['uno', 'des', 'tres'])
-var_dic_call(1, **var_dic)
-var_dic_call(**{"arg1": "uno", "arg2": "dos", "arg3": "tres"})
-test_var_args(1, "two", 3)
-test_var_kwargs(farg=1, myarg2="two", myarg3=3)
-
+#out:
+#('args_pass', {'args': (1, 'two', 3)})
+#('kwargs_pass', {'kwargs': {'arg1': 1, 'arg2': 2, 'arg3': 'three'}})
+#('args_kwargs_pass', {'args': (1, 'two', 3), 'fargs': 1, 'kwargs': {'arg1': 1, 'arg2': 2, 'arg3': 'three'}})
