fea19b14763ecb389a9c1ba6bd8303bf0afe15e3
Alexey Martishin <alex.martishin@yandex.ru>
2016-01-09 23:46:04 +0300

    Update example.py

--- yield_tutorial/example.py
+++ yield_tutorial/example.py
@@ -47,3 +47,10 @@ def print_successive_primes(iterations, base = 10):
 if __name__ == '__main__':
 #    solve_number_10()
     print_successive_primes(2)
+    
+#Simplifying. But remember that yield is a generator
+#1) Insert a line result = [] at the start of the function.
+#2) Replace each yield expr with result.append(expr).
+#3) Insert a line return result at the bottom of the function.
+#4) Yay - no more yield statements! Read and figure out code.
+#5) Compare function to original definition.
