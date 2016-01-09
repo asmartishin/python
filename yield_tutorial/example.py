dc3549d3fd8c22dffca2e8c2cd4042cb07b9ef18
Alexey Martishin <alex.martishin@yandex.ru>
2016-01-09 22:14:55 +0300

    Update example.py

--- yield_tutorial/example.py
+++ yield_tutorial/example.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python
 
-def foo():
-    for i in xrange(4):
-        yield i
+import math
 
-for i in foo():
-    print i
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0:
+                return False
+        return True
+    return False
+
+#def get_primes(input_list):
+#    return (element for element in input_list if is_prime(element))
+
+def get_primes(number):
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+
+def get_primes_send(number):
+    while True:
+        if is_prime(number):
+            number = yield number
+        number += 1
+
+def solve_number_10():
+    total = 2
+    for next_prime in get_primes(3):
+        if next_prime < 2000000:
+            total += next_prime
+        else:
+            print(total)
+            return
+
+def print_successive_primes(iterations, base = 10):
+    prime_generator = get_primes_send(base)
+    prime_generator.send(None)
+    for power in range(iterations):
+        print(prime_generator.send(base ** power))
+
+if __name__ == '__main__':
+#    solve_number_10()
+    print_successive_primes(2)
