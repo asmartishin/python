fd417c4bc75033b4e3f479908f04d0353d67eeea
Alexey Martishin <alex.martishin@yandex.ru>
2016-10-05 08:24:42 +0300

    Create xml_to_dict.py

---
+++ xml_to_dict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from xml.etree import cElementTree as ET
+from pprint import pprint
+from collections import defaultdict
+
+def etree_to_dict(t):
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.items():
+                dd[k].append(v)
+        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.items()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v) for k, v in t.attrib.items())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = text
+    return d
+
+
+f = open('stat')
+
+x = ET.XML(f.read())
+
+pprint(etree_to_dict(x))
