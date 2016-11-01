9de51229ebf34acdd86e6eb46813cfa18c691550
Alexey Martishin <asmartishin@gmail.com>
2016-11-01 19:00:03 +0300

    Create parse_url.py

---
+++ parse_url.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import urllib.parse as urlparse
+import sys
+import base64
+
+for url in sys.stdin:
+    print(dict(urlparse.parse_qsl(urlparse.urlsplit(url).query)))
+
+for flag in sys.stdin:
+    try:
+        print(list(map(lambda x: base64.b64decode(bytes(x, 'utf-8')),flag.split(','))))
+    except Exception:
+        pass
