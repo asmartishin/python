d59fef4da305e8b2432587468ecb85f3db0a3ff3
Alexey Martishin <asmartishin@gmail.com>
2017-03-06 17:33:52 +0300

    Update negative_regexp.py

--- negative_regexp.py
+++ negative_regexp.py
@@ -7,7 +7,6 @@ def main(letters):
     do_not_like = re.compile('^((?!like).)*$')
     return [x for x in letters if do_not_like.match(x)]
 
-
 if __name__ == '__main__':
     letters = ['You hate cs', "he like's cs", 'I like cs']
     print(main(letters))
