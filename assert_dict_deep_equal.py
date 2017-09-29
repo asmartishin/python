bb56459c4234614bfae638717154b27fa8ba5fbe
Alex Martishin <asmartishin@gmail.com>
2017-09-29 20:40:49 +0300

    Update assert_dict_deep_equal.py

--- assert_dict_deep_equal.py
+++ assert_dict_deep_equal.py
@@ -1,41 +1,41 @@
-    def assertDeepAlmostEqual(self, expected, actual, *args, **kwargs):
-        """
-        Assert that two complex structures have almost equal contents.
+def assertDeepAlmostEqual(self, expected, actual, *args, **kwargs):
+    """
+    Assert that two complex structures have almost equal contents.
 
-        Compares lists, dicts and tuples recursively. Checks numeric values
-        using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
-        checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
-        Accepts additional positional and keyword arguments and pass those
-        intact to assertAlmostEqual() (that's how you specify comparison
-        precision).
+    Compares lists, dicts and tuples recursively. Checks numeric values
+    using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
+    checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
+    Accepts additional positional and keyword arguments and pass those
+    intact to assertAlmostEqual() (that's how you specify comparison
+    precision).
 
-        :param test_case: TestCase object on which we can call all of the basic
-        'assert' methods.
-        :type test_case: :py:class:`unittest.TestCase` object
-        """
-        is_root = '__trace' not in kwargs
-        trace = kwargs.pop('__trace', 'ROOT')
-        try:
-            if isinstance(expected, (int, float, long, complex)):
-                self.assertAlmostEqual(expected, actual, *args, **kwargs)
-            elif isinstance(expected, (list, tuple, numpy.ndarray)):
-                self.assertEqual(len(expected), len(actual))
-                for index in xrange(len(expected)):
-                    v1, v2 = expected[index], actual[index]
-                    self.assertDeepAlmostEqual(
-                        v1, v2, __trace=repr(index), *args, **kwargs
-                    )
-            elif isinstance(expected, dict):
-                self.assertEqual(set(expected), set(actual))
-                for key in expected:
-                    self.assertDeepAlmostEqual(
-                        expected[key], actual[key], __trace=repr(key), *args, **kwargs
-                    )
-            else:
-                self.assertEqual(expected, actual)
-        except AssertionError as exc:
-            exc.__dict__.setdefault('traces', []).append(trace)
-            if is_root:
-                trace = ' -> '.join(reversed(exc.traces))
-                exc = AssertionError("%s\nTRACE: %s" % (exc.message, trace))
-            raise exc
+    :param test_case: TestCase object on which we can call all of the basic
+    'assert' methods.
+    :type test_case: :py:class:`unittest.TestCase` object
+    """
+    is_root = '__trace' not in kwargs
+    trace = kwargs.pop('__trace', 'ROOT')
+    try:
+        if isinstance(expected, (int, float, long, complex)):
+            self.assertAlmostEqual(expected, actual, *args, **kwargs)
+        elif isinstance(expected, (list, tuple, numpy.ndarray)):
+            self.assertEqual(len(expected), len(actual))
+            for index in xrange(len(expected)):
+                v1, v2 = expected[index], actual[index]
+                self.assertDeepAlmostEqual(
+                    v1, v2, __trace=repr(index), *args, **kwargs
+                )
+        elif isinstance(expected, dict):
+            self.assertEqual(set(expected), set(actual))
+            for key in expected:
+                self.assertDeepAlmostEqual(
+                    expected[key], actual[key], __trace=repr(key), *args, **kwargs
+                )
+        else:
+            self.assertEqual(expected, actual)
+    except AssertionError as exc:
+        exc.__dict__.setdefault('traces', []).append(trace)
+        if is_root:
+            trace = ' -> '.join(reversed(exc.traces))
+            exc = AssertionError("%s\nTRACE: %s" % (exc.message, trace))
+        raise exc
