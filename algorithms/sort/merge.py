54a04c255469004212a162f35f4cc2bff51ada30
asmartishin <asmartishin@gmail.com>
2017-12-17 06:45:05 +0400

    merge sort

--- algorithms/sort/merge.py
+++ algorithms/sort/merge.py
@@ -1,35 +1,52 @@
-def merge_sort(alist):
-    print('Splitting ', alist)
-    if len(alist) > 1:
-        mid = len(alist) // 2
+#!/usr/bin/env python
 
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
 
-        merge_sort(lefthalf)
-        merge_sort(righthalf)
-        i, j, k = 0, 0, 0
+def merge_sort(nums, l, r):
+    if l < r:
+        m = (l + r) / 2
+        merge_sort(nums, l, m)
+        merge_sort(nums, m + 1, r)
+        merge(nums, l, m, r)
 
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                alist[k] = lefthalf[i]
-                i += 1
-            else:
-                alist[k] = righthalf[j]
-                j += 1
-            k += 1
 
-        while i < len(lefthalf):
-            alist[k] = lefthalf[i]
-            i += 1
-            k += 1
+def merge(nums, l, m, r):
+    li = 0
+    ri = 0
+    left_part = []
+    right_part = []
 
-        while j < len(righthalf):
-            alist[k] = righthalf[j]
-            j += 1
-            k += 1
-    print('Merging ', alist)
+    for i in range(l, r + 1):
+        if i <= m:
+            left_part.append(nums[i])
+        else:
+            right_part.append(nums[i])
 
-alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-merge_sort(alist)
-print(alist)
+    while li < len(left_part) and ri < len(right_part):
+        if left_part[li] <= right_part[ri]:
+            nums[l] = left_part[li]
+            li += 1
+        else:
+            nums[l] = right_part[ri]
+            ri += 1
+        l += 1
+
+    while li < len(left_part):
+        nums[l] = left_part[li]
+        l += 1
+        li += 1
+
+    while ri < len(right_part):
+        nums[l] = right_part[ri]
+        l += 1
+        ri += 1
+
+
+def main():
+    nums = [1, 3, 2, 6, 7, 5]
+    merge_sort(nums, 0, len(nums) - 1)
+    print nums
+
+
+if __name__ == '__main__':
+    main()
+
