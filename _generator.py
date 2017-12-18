1cf2ac49924e822bac4292aee91ed80395dbc73d
asmartishin <asmartishin@gmail.com>
2017-12-18 04:10:17 +0400

    generator

---
+++ _generator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+from collections import deque
+
+
+def numbers():
+    number = 1
+    while True:
+        d = deque(str(number))
+        while d:
+            yield d.popleft()
+        number += 1
+
+
+def take(n, seq):
+    seq = iter(seq)
+    result = ''
+    try:
+        for i in range(n):
+            result += seq.next()
+    except StopIteration:
+        pass
+    return result
+
+
+def main():
+    print take(10, numbers())
+
+
+if __name__ == '__main__':
+    main()
