9bd1562f8f13a53d8f7b454e240daa6e449bdcab
Alex Martishin <asmartishin@gmail.com>
2018-07-05 18:20:21 +0300

    post_listener

---
+++ post_listener.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import logging
+from sys import stdout
+import argparse
+
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=8080)
+    return parser.parse_args()
+
+
+@app.route('/', methods=['POST'])
+def root():
+    data = request.get_json(force=True)
+    app.logger.info(data)
+    return Response(status=200)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    app.run(host='::', port=args.port, debug=True)
+
